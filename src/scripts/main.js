b7f553a8528275302d5c5c9e722679c37c08dac4
3vli <om1ak2345@gmail.com>
2024-12-27 18:07:46 +0200

    Hero Section Logic

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,24 +1,12 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
-
-setupCounter(document.querySelector('#counter'))
+let i = 0;
+let txt = `هلا بك في موقع شبكة الإبداع، هنا حيث يجتمع الإبداع والمهارات في هذا المكان المميز. وكذلك يمكنك مشاهدة فريق المطورين والمصممين من خلال الضغط على 'الفريق'.`
+let speed = 50;
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("demo").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
+typeWriter();
