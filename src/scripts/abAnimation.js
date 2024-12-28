401e4c4b210024e7840633098623cc266f8a6e99
3vli <om1ak2345@gmail.com>
2024-12-28 23:20:52 +0200

    about page

---
+++ src/scripts/abAnimation.js
@@ -0,0 +1,12 @@
+const aboutCircle = document.querySelector('#aboutCircle');
+
+aboutCircle.addEventListener('animationiteration', () => {
+   const randomTop = Math.random() * 100;
+   aboutCircle.style.top = `${randomTop}%`;
+})
+const aboutCircler = document.querySelector('#aboutCircler');
+
+aboutCircler.addEventListener('animationiteration', () => {
+   const randomTop = Math.random() * 100;
+   aboutCircler.style.top = `${randomTop}%`;
+})
