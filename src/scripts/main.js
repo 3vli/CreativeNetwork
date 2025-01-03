e84d7f539904a36b4c62a3a4bac915d0aa3c4993
3vli <om1ak2345@gmail.com>
2025-01-04 00:44:49 +0200

    rechange design and make header and hero sec

--- src/scripts/main.js
+++
@@ -1,9 +0,0 @@
-let menuButton = document.getElementById("toggleMenu");
-let sidebar = document.querySelector(".sidebar");
-let closeButton = document.getElementById("closeIco")
-menuButton.addEventListener("click", () => {
-   sidebar.style.display = `flex`;
-})
-closeButton.addEventListener("click", () => {
-   sidebar.style.display = `none`;
-})
